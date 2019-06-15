55bd7588ce93e01ff91c024637045b201aca614d
Sridhar Anandakrishnan <sak@essc.psu.edu>
2019-06-14 22:16:28 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# seg
 # seg2segy
+
+command line program to convert SEG-2 files to SU (seismic unix) pseudo SEG-Y format. Written in go (htts://golang.org)
+
+Usage: seg2segy [-o outfile] [-n nfiles] nnn.dat [mmm.dat ...]
+
+Either specify a list of one or more seg2 files (nnn.dat mmm.dat ...) or specify one file (`nnn.dat`) and `-n nfiles` to process consecutive files (nnn, nnn+1 ... nnn+nfiles-1)
+
+By default outfile is `nnn.su` unless `-o outfile` is specified.
+
+Many data acquisition systems (e.g., Geometrics Geode) generate consecutively numbered files.
+
+Seismic Unix: https://cwp.mines.edu/software  
+https://github.com/JohnWStockwellJr/SeisUnix  
+
+# Install
+
+  - go get github.com/sanandak/seg2segy
+  - cd <seg2segy> dir
+  - go get github.com/sanandak/seg
+  - go build
+  - go install
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/sanandak/seg/seg2"
+	"github.com/sanandak/seg/segy"
+)
+
+func main() {
+	outfile := flag.String("o", "", "su output file name")
+	nf := flag.Int("n", 1, "number of consecutive files to process")
+	flag.Parse()
+	files := flag.Args()
+
+	if len(files) == 0 {
+		log.Fatal("no input files specified")
+	}
+	// seg2 files are consecutive number nnn.dat
+	// -n nf says process nnn, nnn+1...nnn+nf-1
+	f0 := files[0]
+	f0num, err := strconv.Atoi(strings.TrimSuffix(f0, filepath.Ext(f0)))
+	fmt.Println(files, *nf)
+	if len(*outfile) == 0 {
+		fnoex := strings.TrimSuffix(f0, filepath.Ext(f0))
+		*outfile = fnoex + ".su"
+	}
+
+	fout, err := os.OpenFile(*outfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal("err opening su outfile", *outfile)
+	}
+	defer fout.Close()
+	for i := 0; i < *nf; i++ {
+		fn := fmt.Sprintf("%d.dat", f0num+i)
+		fmt.Println("processing file ", i, fn)
+
+		seg2trcs := seg2.ReadSEG2(fn)
+		//fmt.Println(len(seg2trcs))
+		//fmt.Println(seg2trcs[0].Data[:200])
+
+		for _, t := range seg2trcs {
+			segytrc := segy.SEG2SEGY(t)
+			//fmt.Println(len(segytrc))
+			n, err := fout.Write(segytrc)
+			if err != nil {
+				fmt.Println("err writing su file", n, err)
+			}
+			//fmt.Printf("%T\n", segytrc.TraceHeader)
+			//binary.Write(fout, binary.LittleEndian, segytrc.TraceHeader)
+		}
+	}
+}
